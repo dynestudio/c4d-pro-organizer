c46a1d6de2ad78d334fcd78e63af2066b32a3a16
Dyne Estudio <40637572+dynestudio@users.noreply.github.com>
2019-11-21 20:50:08 -0300

    v0.3

--- develop/Add Divider Group.py
+++ develop/Add Divider Group.py
@@ -1,5 +1,8 @@
 import c4d
  
+ 
+color_divider=c4d.Vector(1,1,1) # Layer Color
+ 
 def add_divider(name, color):
 
 
@@ -37,8 +40,50 @@ def add_divider(name, color):
        doc.InsertObject(Null)
        
        c4d.EventAdd()
+       
+def insert_up():
+      ActiveDocument = doc.GetDocument()
+      ObjectsList = doc.GetObjects()
+      FirstObject = doc.GetFirstObject()
+      ActiveObject = doc.GetActiveObject()
+      FirstObject.InsertAfter(ActiveObject)
 
+def insert_down():
+      ActiveDocument = doc.GetDocument()
+      ObjectsList = doc.GetObjects()
+      FirstObject = doc.GetFirstObject()
+      ActiveObject = doc.GetActiveObject()
+      FirstObject.InsertBefore(ActiveObject)
 
-color_divider=c4d.Vector(1,1,1) # Layer Color
-add_divider("_dividers_",color_divider)
+def insert_child():
+      ActiveDocument = doc.GetDocument()
+      ObjectsList = doc.GetObjects()
+      FirstObject = doc.GetFirstObject()
+      ActiveObject = doc.GetActiveObject()
+      FirstObject.InsertUnder(ActiveObject)
 
+def Shorcuts_Functions():
+
+    doc = c4d.documents.GetActiveDocument()
+    bc = c4d.BaseContainer()
+
+    if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+        if  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QSHIFT: 
+            add_divider("_dividers_",color_divider)
+            insert_up()
+             
+        elif  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QALT: 
+            add_divider("_dividers_",color_divider)
+            insert_down()
+            
+        elif  bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QCTRL: 
+            add_divider("_dividers_",color_divider)
+            insert_child()
+             
+        else:
+            add_divider("_dividers_",color_divider)
+
+    c4d.EventAdd()
+
+if __name__=='__main__':
+    Shorcuts_Functions()
